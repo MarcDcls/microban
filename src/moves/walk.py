ee796fdaeb05a34cc05c0bb1a675a82f28053bfb
MarcDcls <marc.duclusaud@gmail.com>
2026-06-23 09:28:59 +0200

    Adding reference phase to walk move

--- src/moves/walk.py
+++ src/moves/walk.py
@@ -1,9 +1,14 @@
 import onnxruntime as ort
+import numpy as np
 
 from constants import MOTOR_TO_ID, KP_DEFAULT, KP_RL, OBSERVATION_DOF_ORDER, NEUTRAL_POSE
 from controller import ControllerProtocol
 from observer import Observation
 from moves.move import MotorCommand, Move, MoveState
+
+
+USE_REFERENCE_PHASE = True
+LOGGING = False
 
 
 class WalkMove(Move):
@@ -17,52 +22,56 @@ class WalkMove(Move):
         # Load ONNX policy
         self._ort_session = ort.InferenceSession("src/agents/walk.onnx")
 
+        # Reference phase for gait cycle
+        self._phase_step = 0
+        self._phase_total_steps = 20
+
         # Safety parameters
         self._projected_gravity_z_threshold = -0.5  # Threshold for detecting a fall based on projected gravity
 
         # Logging
-        # self.position = {
-        #     "head": [],
-        #     "left_hip_yaw": [],
-        #     "left_hip_roll": [],
-        #     "left_hip_pitch": [],
-        #     "left_knee": [],
-        #     "left_ankle_pitch": [],
-        #     "left_ankle_roll": [],
-        #     "right_hip_yaw": [],
-        #     "right_hip_roll": [],
-        #     "right_hip_pitch": [],
-        #     "right_knee": [],
-        #     "right_ankle_pitch": [],
-        #     "right_ankle_roll": [],
-        #     "left_shoulder_pitch": [],
-        #     "left_shoulder_roll": [],
-        #     "left_elbow": [],
-        #     "right_shoulder_pitch": [],
-        #     "right_shoulder_roll": [],
-        #     "right_elbow": [],
-        # }
-        # self.voltage = {
-        #     "head": [],
-        #     "left_hip_yaw": [],
-        #     "left_hip_roll": [],
-        #     "left_hip_pitch": [],
-        #     "left_knee": [],
-        #     "left_ankle_pitch": [],
-        #     "left_ankle_roll": [],
-        #     "right_hip_yaw": [],
-        #     "right_hip_roll": [],
-        #     "right_hip_pitch": [],
-        #     "right_knee": [],
-        #     "right_ankle_pitch": [],
-        #     "right_ankle_roll": [],
-        #     "left_shoulder_pitch": [],
-        #     "left_shoulder_roll": [],
-        #     "left_elbow": [],
-        #     "right_shoulder_pitch": [],
-        #     "right_shoulder_roll": [],
-        #     "right_elbow": [],
-        # }
+        self.position = {
+            "head": [],
+            "left_hip_yaw": [],
+            "left_hip_roll": [],
+            "left_hip_pitch": [],
+            "left_knee": [],
+            "left_ankle_pitch": [],
+            "left_ankle_roll": [],
+            "right_hip_yaw": [],
+            "right_hip_roll": [],
+            "right_hip_pitch": [],
+            "right_knee": [],
+            "right_ankle_pitch": [],
+            "right_ankle_roll": [],
+            "left_shoulder_pitch": [],
+            "left_shoulder_roll": [],
+            "left_elbow": [],
+            "right_shoulder_pitch": [],
+            "right_shoulder_roll": [],
+            "right_elbow": [],
+        }
+        self.voltage = {
+            "head": [],
+            "left_hip_yaw": [],
+            "left_hip_roll": [],
+            "left_hip_pitch": [],
+            "left_knee": [],
+            "left_ankle_pitch": [],
+            "left_ankle_roll": [],
+            "right_hip_yaw": [],
+            "right_hip_roll": [],
+            "right_hip_pitch": [],
+            "right_knee": [],
+            "right_ankle_pitch": [],
+            "right_ankle_roll": [],
+            "left_shoulder_pitch": [],
+            "left_shoulder_roll": [],
+            "left_elbow": [],
+            "right_shoulder_pitch": [],
+            "right_shoulder_roll": [],
+            "right_elbow": [],
+        }
         
     def on_start(self, obs: Observation, command: MotorCommand) -> None:
         if self._controller is not None:
@@ -71,13 +80,20 @@ class WalkMove(Move):
         self.state = MoveState.ACTIVE
 
     def step(self, obs: Observation, command: MotorCommand) -> None:
-        input_obs = self.build_observation(obs)
+        # Update reference phase
+        if USE_REFERENCE_PHASE:
+            commanded_vel = np.mean([np.abs(obs.user_input.velocity["vx"]), np.abs(obs.user_input.velocity["vy"]), np.abs(obs.user_input.velocity["vtheta"])])
+            if commanded_vel > 0.01:
+                self._phase_step += 1
+            else:
+                self._phase_step = 0
 
         # Safety check: if the robot is fallen, stop the policy
         if obs.robot_state.projected_gravity[2] > self._projected_gravity_z_threshold:
             return
         
         # Run policy
+        input_obs = self.build_observation(obs)
         ort_inputs = {self._ort_session.get_inputs()[0].name: [input_obs]}
         ort_outs = self._ort_session.run(None, ort_inputs)
         action = ort_outs[0][0]
@@ -88,9 +104,10 @@ class WalkMove(Move):
             command.target_angles[name] = NEUTRAL_POSE[name] + action[i]
 
         # Log positions and voltages
-        # for name in MOTOR_TO_ID.keys():
-        #     self.position[name].append(obs.robot_state.motor_positions[name])
-        #     self.voltage[name].append(obs.robot_state.motor_voltages[name])
+        if LOGGING:
+            for name in MOTOR_TO_ID.keys():
+                self.position[name].append(obs.robot_state.motor_positions[name])
+                self.voltage[name].append(obs.robot_state.motor_voltages[name])
 
     def build_observation(self, obs: Observation) -> list[float]:
         """Build policy observation from robot state."""
@@ -116,6 +133,12 @@ class WalkMove(Move):
         input_obs.append(obs.user_input.velocity["vy"])
         input_obs.append(obs.user_input.velocity["vtheta"])
 
+        # Reference phase
+        if USE_REFERENCE_PHASE:
+            reference_phase = (self._phase_step % self._phase_total_steps) / self._phase_total_steps * 2 * np.pi
+            input_obs.append(np.cos(reference_phase))
+            input_obs.append(np.sin(reference_phase))
+
         return input_obs
 
     def on_stop(self, obs: Observation, command: MotorCommand) -> None:
@@ -125,9 +148,10 @@ class WalkMove(Move):
         self.state = MoveState.INACTIVE
 
         # Save json logs
-        # import json
-        # with open("walk_log.json", "w") as f:
-        #     json.dump({
-        #         "position": self.position,
-        #         "voltage": self.voltage,
-        #     }, f, indent=4)
+        if LOGGING:
+            import json
+            with open("walk_log.json", "w") as f:
+                json.dump({
+                    "position": self.position,
+                    "voltage": self.voltage,
+                }, f, indent=4)
